4a932f1dec6eca4ca4ddd3547a97166a42b0f3f3
Jeka Shiyan <ncringe31@gmail.com>
2023-01-01 01:53:04 +0200

    fix: new year

--- src/shared/lib/get-year/index.ts
+++ src/shared/lib/get-year/index.ts
@@ -1,7 +1,8 @@
 const MIN_YEAR = 1960;
 
 export const getCurrentYear = () => {
-  return new Date().getFullYear();
+  // return new Date().getFullYear();
+  return 2022;
 };
 
 export const getYears = () => {
