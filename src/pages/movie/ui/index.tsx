f3eba38fcc7e64b933b356938c6038e306ceb2a8
Jeka Shiyan <ncringe31@gmail.com>
2023-01-08 13:21:25 +0200

    fix: some styles fixess

--- src/pages/movie/ui/index.tsx
+++ src/pages/movie/ui/index.tsx
@@ -2,22 +2,43 @@ import clsx from 'clsx';
 import type { NextPage } from 'next';
 import { useStore } from 'effector-react';
 import { pageModel, getGenre, getMovieBreadcrumb } from 'pages/movie';
-import { Breadcrumbs } from 'shared/ui/breadcrumbs';
+import { Breadcrumbs, Title } from 'shared/ui';
 import { Player } from './player';
 import { Info } from './info';
+import { MainPersons } from './main-persons';
+import { Description } from './description';
 import styles from './styles.module.scss';
+import { Rating } from './rating';
 
 export const Movie: NextPage = () => {
-  const { genres, type } = useStore(pageModel.$movie)!;
+  const { genres, type, name } = useStore(pageModel.$movie)!;
   const breadcrumbs = [getMovieBreadcrumb(type), getGenre(genres)];
 
   return (
     <section className={styles.section}>
       <div className={clsx('container', styles.container)}>
         <Breadcrumbs className={styles.breadcrumbs} items={breadcrumbs} />
-        <div className={styles.content}>
-          <Player />
+        <div className={styles.mobile}>
+          <Title className={styles.title}>
+            <span>{name} Смотреть онлайн</span>
+          </Title>
           <Info />
+        </div>
+        <div className={styles.wrapper}>
+          <Player />
+          <div className={styles.info}>
+            <div className={styles.desktop}>
+              <Title className={styles.title}>
+                <span>{name} Смотреть онлайн</span>
+              </Title>
+              <Info />
+            </div>
+            <div className={styles.content}>
+              <MainPersons />
+              <Description />
+              <Rating />
+            </div>
+          </div>
         </div>
       </div>
     </section>
