c13d179671124b532fffe2163fd6ad6c2a6912a8
Jeka Shiyan <ncringe31@gmail.com>
2023-01-09 16:17:35 +0200

    feat: loading spinner for video trailer

--- src/pages/movie/ui/player/ui/trailer-modal/ui.tsx
+++ src/pages/movie/ui/player/ui/trailer-modal/ui.tsx
@@ -1,23 +1,37 @@
 import clsx from 'clsx';
+import { useState } from 'react';
 import { useStore } from 'effector-react';
 import { pageModel } from 'pages/movie';
 import { useToggler } from 'shared/lib/hooks';
-import { CloseIcon, Modal } from 'shared/ui';
+import { CloseIcon, Modal, Spinner } from 'shared/ui';
 import styles from './styles.module.scss';
 
 export const TrailerModal = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const { videos } = useStore(pageModel.$movie)!;
   const { close, isOpen } = useToggler(pageModel.trailerModalInstance);
   const trailerUrl = videos?.trailers[videos?.trailers?.length - 1]?.url;
+  const hasTrailers = videos!.trailers.length > 0;
+
+  const handleClose = () => {
+    setIsLoading(true);
+    close();
+  };
 
   return (
-    <Modal onClick={close} className={styles.modal} isOpen={isOpen} close={close}>
+    <Modal onClick={handleClose} className={styles.modal} isOpen={isOpen} close={handleClose}>
       <div className={styles.container}>
-        <button onClick={close} className={clsx('btn-reset', styles.close)}>
+        <button onClick={handleClose} className={clsx('btn-reset', styles.close)}>
           <CloseIcon />
         </button>
         <div className={styles.content}>
+          {isLoading && hasTrailers && (
+            <div className={styles.spinner}>
+              <Spinner strokeWidth={2} />
+            </div>
+          )}
           <iframe
+            onLoad={() => setIsLoading(false)}
             className={styles.iframe}
             src={trailerUrl}
             title="Трейлер"
