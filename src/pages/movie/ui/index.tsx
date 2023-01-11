25e5cf252ed1cb490fa502276ae09cf27f1cdf33
Jeka Shiyan <ncringe31@gmail.com>
2023-01-11 17:24:39 +0200

    fix: some fixes

--- src/pages/movie/ui/index.tsx
+++ src/pages/movie/ui/index.tsx
@@ -1,12 +1,26 @@
+import Head from 'next/head';
 import type { NextPage } from 'next';
+import { useStore } from 'effector-react';
+import { pageModel } from 'pages/movie';
 import { Persons } from './persons';
 import { SimilarMovies } from './similar-movies';
 import { MainSection } from './main-section';
 import { Facts } from './facts';
 
 export const Movie: NextPage = () => {
+  const data = useStore(pageModel.$movie);
+
   return (
     <>
+      <Head>
+        <meta name="description" content={data?.description} />
+        <title>
+          {data?.name} ({data?.year}) смотреть онлайн бесплатно в хорошем HD 1080 / 720 качестве
+        </title>
+        <meta property="og:title" content={`${data?.name} (${data?.year})`} />
+        <meta property="og:description" content={data?.description} />
+        <meta property="og:image" content={data?.poster?.url} />
+      </Head>
       <MainSection />
       <SimilarMovies />
       <Persons />
