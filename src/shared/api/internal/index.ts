b4ddcd2c2865f7cde018853b7eb55733d9c54ec7
Shiyan7 <jeka.shiyan@gmail.com>
2023-01-18 10:38:44 +0200

    fix: fix axios error

--- src/shared/api/internal/index.ts
+++ src/shared/api/internal/index.ts
@@ -2,13 +2,11 @@ import { http } from './base';
 import type { HeroMovie } from './types';
 
 const routesConfig = http.createRoutesConfig({
-  getHeroMovies: http.createRoute<void, HeroMovie[]>(() => ({
+  getHeroMovies: http.createRoute<void, HeroMovie[]>({
     url: '/hero',
-    method: 'get',
-  })),
+  }),
   check: http.createRoute<string, { status: boolean }>((email) => ({
     url: '/check',
-    method: 'get',
     params: {
       email,
     },
