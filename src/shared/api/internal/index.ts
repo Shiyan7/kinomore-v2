e6a2855a1b0a339ee907dee543f54dc482b7431a
Shiyan7 <jeka.shiyan@gmail.com>
2023-01-18 10:42:54 +0200

    fix

--- src/shared/api/internal/index.ts
+++ src/shared/api/internal/index.ts
@@ -3,6 +3,9 @@ import type { HeroMovie } from './types';
 
 const routesConfig = http.createRoutesConfig({
   getHeroMovies: http.createRoute<void, HeroMovie[]>({
+    headers: {
+      'Content-Type': 'application/json',
+    },
     url: '/hero',
   }),
   check: http.createRoute<string, { status: boolean }>((email) => ({
